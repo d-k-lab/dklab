89da39d86a1924304e800dbe5a4b9ae889d0f8a6
DKLAB <drlee@dklab.kr>
2023-03-08 13:32:36 +0900

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
     <footer>
+      <p>ⓒ DKLAB All Rights Reserved.</p>
       <ul>
         <li><p>상호명 : 디케이랩</p><p>사업장등록번호 : 142-49-00550</p><p>대표자 : 고정석</p></li>
         <li><p>대표번호 : 070-4354-0782</p><p>팩스 : 070-7543--0709</p></li>
         <li><p>주소 : 경기도 성남시 성남대로331번길 8, 19층 14호 디케이랩</p></li>
       </ul>
-      <p>ⓒ DKLAB All Rights Reserved.</p>
     </footer>
   )
 }
