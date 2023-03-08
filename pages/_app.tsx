ae9c8a9bb16c6f09cbf4dbc53c70395423c62c15
DKLAB <drlee@dklab.kr>
2023-03-08 11:21:15 +0900

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,48 @@
 import '@/styles/styles.scss'
+import * as gtag from './api/ga';
+import Script from 'next/script';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <>
       <Head>
         <title>DKLAB 디케이랩</title>
         <link rel="icon" href="/image/icon/dklab-favicon.ico" />
       </Head>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
