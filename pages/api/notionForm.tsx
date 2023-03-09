f2bdb2b321499ee6ec7d733cf58667577746c199
DKLAB <drlee@dklab.kr>
2023-03-09 11:33:58 +0900

    스마트공방 신청 폼

---
+++ pages/api/notionForm.tsx
@@ -0,0 +1,81 @@
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({
+  auth: process.env.NOTION_FORM_TOKEN,
+});
+
+export default async function handler(req:any, res:any) {
+  console.log('reqqqqqqqq %o', req);
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .json({ message: `${req.method} requests are not allowed` });
+  }
+  try {
+    const { name, phone, email, check, company, employ, category } = JSON.parse(req.body);
+    await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_FORM_DATABASE_ID,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+        Phone: {
+          rich_text: [
+            {
+              text: {
+                content: phone,
+              },
+            },
+          ],
+        },
+        Email: {
+          rich_text: [
+            {
+              text: {
+                content: email,
+              },
+            },
+          ],
+        },
+        Company: {
+          rich_text: [
+            {
+              text: {
+                content: company,
+              },
+            },
+          ],
+        },
+        Employ: {
+          rich_text: [
+            {
+              text: {
+                content: employ,
+              },
+            },
+          ],
+        },
+        Category: {
+          rich_text: [
+            {
+              text: {
+                content: category,
+              },
+            },
+          ],
+        },
+      },
+    });
+    res.status(201).json({ msg: 'Success' });
+  } catch (error) {
+    res.status(500).json({ msg: 'There was an error' });
+  }
+}
--- pages/vendor/smartStudio.tsx
+++ pages/vendor/smartStudio.tsx
@@ -7,8 +7,55 @@ import vendor_s from '@/public/image/vendor/smart-s.png';
 import vendor_s_map from '@/public/image/vendor/smart-s-map.png';
 import vendor_s_icon from '@/public/image/vendor/smart-s-icon.png';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function SmartStudio() {
+  const [form, setForm] = useState(false);
+  const [agree, setAgree] = useState(true);
+  const [company, setCompany] = useState('');
+  const [category, setCategory] = useState('');
+  const [employ, setEmploy] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [check, setCheck] = useState('');
+  // 신청하기 버튼 클릭
+  const smartForm = () => {
+    setForm(true);
+    console.log('Smart Studio form');
+  }
+  // 동의하기 버튼 클릭
+  const agreeBtn = (e:any) => {
+    setAgree((prev) => !prev);
+  }
+  // 인풋 텍스트
+  const handleCompany = (e:any) => {setCompany(e.target.value);}
+  const handleCategory = (e:any) => {setCategory(e.target.value);}
+  const handleEmploy = (e:any) => {setEmploy(e.target.value);}
+  const handleName = (e:any) => setName(e.target.value);
+  const handlePhone = (e:any) => {setPhone(e.target.value);}
+  const handleEmail = (e:any) => {setEmail(e.target.value);}
+  // const handleCheck = (e:any) => {setCheck(e.target.value);}
+  // const handleSubmit = (e:any) => {
+  //   e.preventDefault();
+  //   console.log(`${agree}`);
+  // }
+
+  const submitForm = async (e:any) => {
+    e.preventDefault();
+    const res = await fetch('http://localhost:3000/api/notionForm', {
+      method: 'POST',
+      body: JSON.stringify({ name, phone, email, company, employ, category  }),
+    });
+    // Success if status code is 201
+    if (res.status === 201) {
+      console.log('Thank you for contacting us!', { type: 'success' });
+    } else {
+      console.log('Please re-check your inputs.', { type: 'error' });
+    }
+  };
+
   return (
     <>
     <Head>
@@ -25,23 +72,57 @@ export default function SmartStudio() {
     <div className='smartProduct'>
       <Header><h1>스마트공방</h1></Header>
       <main>
-          <section>
-            <div>
-              <Image src={vendor_s_icon} alt='스마트공방'/>
-            </div>
-            <div>
-              <p className='title'>스마트 공방이란</p>
-              <p className='discription'>‘스마트공방’은 소공인(10인 미만 제조업)이 스마트기술(IoT, AI 등)을 도입해 수작업 위주의 제조공정을 개선(부분 자동화, 생산관리시스템 도입 등)하고 생산성과 품질 향상하도록 지원하는 사업입니다.</p>
-            </div>
-          </section>
-          <section>
-            <p className='subtitle'>스마트 공방</p>
+        <section>
+          <div>
+            <Image src={vendor_s_icon} alt='스마트공방'/>
+          </div>
+          <div>
+            <p className='title'>스마트 공방이란</p>
+            <p className='discription'>‘스마트공방’은 소공인(10인 미만 제조업)이 스마트기술(IoT, AI 등)을 도입해 수작업 위주의 제조공정을 개선(부분 자동화, 생산관리시스템 도입 등)하고 생산성과 품질 향상하도록 지원하는 사업입니다.</p>
+          </div>
+        </section>
+        <section>
+          <p className='subtitle'>스마트 공방</p>
+          <div className='performance'>
             <div>
               <Image src={vendor_s} alt="스마트공방 실적"/>
-              <Image src={vendor_s_map} alt="스마트공방 실적"/>
+              <button onClick={smartForm}>스마트 공방 신청하기</button>
             </div>
-          </section>
-          <section></section>
+            <Image src={vendor_s_map} alt="스마트공방 실적"/>
+          </div>
+        </section>
+        <section>
+          {form
+            ? (
+              <form onSubmit={submitForm}>
+                <input type="hidden" name="dklab-form" value="contact" />
+                <p>스마트공방 상담 신청</p>
+                <label>기업명       <input type='text'  id='company' name='company' value={company} onChange={handleCompany} required></input></label>
+                <label>업종 및 업태  <input type='text' id='category' name='category'  value={category} onChange={handleCategory} required></input></label>
+                <label>근로자 수     <input type='text' id='employ' name='employ'  value={employ} onChange={handleEmploy} required></input></label>
+                <hr/>
+                <label>담당자 이름   <input type='text' id='name' name='name' value={name} onChange={handleName} required></input></label>
+                <label>담당자 연락처  <input type='text' id='phone' name='phone' value={phone} onChange={handlePhone} required></input></label>
+                <label>담당자 메일주소 <input type='text' id='email' name='email' value={email} onChange={handleEmail} required></input></label>
+                <div className='agree'>
+                  <label htmlFor='agree'>
+                    <input
+                      type='checkbox' name='agree' value='agree'
+                      onClick={agreeBtn}
+                      />
+                    개인정보 동의합니다.
+                  </label>
+                    <span></span>
+                  {agree
+                    ? null
+                    : <button type='submit'>신청</button>
+                  }
+                </div>
+              </form>
+              )
+            : null
+          }
+        </section>
       </main>
       <Footer />
     </div>
